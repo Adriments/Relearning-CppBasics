a2ee4cddb4933a3dd0e2591057c9c26b0c50b014
Adrian Clements <adriments@gmail.com>
2020-08-12 21:04:25 -0500

    Added Episode 4

---
+++ Relearning CppBasics/Episode4.cpp
@@ -0,0 +1,26 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string name = "Randy";
+	cout << name << endl;
+	name = "Jerry"; //Can change strings like you can integers
+	cout << name << endl;
+
+	cout << "Here is my name: " << name << endl; //Can put in multiple strings
+	string sentence = "Here is my name: ";
+	cout << sentence << name << endl;
+
+	string name2 = "bob";
+	string two_names = name + name2; //Called concatenation
+	cout << two_names << endl;
+
+	string book = "Can't Hurt Me";
+	string finalString = "My Favorite book is: " + book;
+	cout << finalString << endl;
+
+	return 0;
+} */
